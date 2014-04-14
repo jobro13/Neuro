d68b6b448990999d839d98534446489074ca57e2
jobro13 <jochembrouwer@telfort.nl>
2014-04-14 19:57:17 +0200

    added initial files

---
+++ README.md
@@ -0,0 +1,3 @@
+=== What is Neuro? === 
+
+Neuro is a Lua library which can be used to create unsupervised learning networks. It uses genetic algorithms to create new "children" to learn.
---
+++ neuro.lua
@@ -0,0 +1,40 @@
+
+
+-- neuron
+local neuron = require "require/neuron"
+
+-- network 
+local network = require "require/network"
+
+-- library
+
+local neurolib = {}
+
+
+
+
+function neurolib.createneuron()
+	return neuron.new()
+end
+
+function neurolib.createnetwork()
+	return network.new()
+end
+
+function neurolib.network()
+	return neurolib.createnetwork()
+end 
+
+function neurolib.neuron()
+	return neurolib.createneuron()
+end 
+
+function neurolib.new(what) 
+	if what == "neuron" then 
+		return neurolib.createneuron()
+	elseif what == "network" then 
+		return neurolib.createnetwork()
+	else 
+		error("Could not create object type: "..tostring(what).." from neuro.")
+	end 
+end 
