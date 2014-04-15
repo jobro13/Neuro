01ac50c7e1e4f826cfa5e72989be23b258d46d26
baklit <j.ed.glossop@gmail.com>
2014-04-15 14:26:12 +0100

    better random

--- Neuro/src/Neuron.lua
+++ Neuro/src/Neuron.lua
@@ -10,7 +10,7 @@ function Neuron.new(numInputs)
 	neuron.weights = {}
 	
 	for i = 1, numInputs + 1 do
-		table.insert(neuron.weights, math.random(1,100) / 100)
+		table.insert(neuron.weights, math.random())
 	end
 
 
